3dfe3465405929523a27f280a70ea9cab74ef9ca
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-10 17:58:46 +0300

    Splash View Body

--- lib/Features/splash/presentation/splash_view.dart
+++ lib/Features/splash/presentation/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/splash/presentation/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -6,6 +7,6 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return const SplashViewBody();
   }
 }
