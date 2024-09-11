4eddd632792c5752a0591162f891a2af4f9a74bf
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-11 15:51:01 +0300

    Custom FeaturedBookListView Added

--- lib/Features/home/presentation/views/widgets/custom_list_view.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view.dart
@@ -1,24 +1,21 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class ListViewItem extends StatelessWidget {
-  const ListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-              image: DecorationImage(
-                image: AssetImage(AssetsApp.test),
-                fit: BoxFit.fill,
-              )),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            image: DecorationImage(
+              image: AssetImage(AssetsApp.test),
+              fit: BoxFit.fill,
+            )),
       ),
     );
   }
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import 'custom_list_view.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -9,8 +11,27 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        ListViewItem(),
+        FeaturedBookListView(),
       ],
+    );
+  }
+}
+
+class FeaturedBookListView extends StatelessWidget {
+  const FeaturedBookListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: FeaturedListViewItem(),
+            );
+          }),
     );
   }
 }
