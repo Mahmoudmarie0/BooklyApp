6d5b3532ca0af3ce23f96f4048b0c03a3f59c5f9
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-11 13:28:32 +0300

    Animation Added

--- lib/Features/splash/presentation/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/widgets/splash_view_body.dart
@@ -2,18 +2,58 @@ import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late AnimationController animate;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    animate = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animate);
+
+    animate.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animate.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset(
-          AssetsApp.logo,
-          width: 50,
-          height: 50,
-        ),
+      body: AnimatedBuilder(
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: Center(
+              child: SvgPicture.asset(
+                AssetsApp.logo,
+                width: 50,
+                height: 50,
+              ),
+            ),
+          );
+        },
+        animation: slidingAnimation,
       ),
     );
   }
