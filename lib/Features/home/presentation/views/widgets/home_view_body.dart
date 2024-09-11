8071a77fe2afeb3277ce01d4985c578b03bb3f8f
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-11 16:27:01 +0300

    BestSellerListViewItem part one Created

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -3,6 +3,8 @@ import 'package:bookly/Features/home/presentation/views/widgets/featured_book_li
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -17,8 +19,37 @@ class HomeViewBody extends StatelessWidget {
           FeaturedBookListView(),
           SizedBox(height: 20),
           Text('Best Seller', style: Styles.titleMedium),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(children: [
+        AspectRatio(
+          aspectRatio: 2.5 / 4,
+          child: Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                image: DecorationImage(
+                  image: AssetImage(AssetsApp.test),
+                  fit: BoxFit.fill,
+                )),
+          ),
+        ),
+        const Column(
+          children: [],
+        ),
+      ]),
     );
   }
 }
