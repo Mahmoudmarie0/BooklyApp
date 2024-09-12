60a71ac12da55cf8d16c3faadff57ec010f5ce09
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-12 15:52:17 +0300

    CustomBookDetailsAppBar Done

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,65 +1,73 @@
 import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../core/utils/app_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 170,
-      child: Row(children: [
-        AspectRatio(
-          aspectRatio: 2.5 / 4,
-          child: Container(
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                image: DecorationImage(
-                  image: AssetImage(AssetsApp.test),
-                  fit: BoxFit.fill,
-                )),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 170,
+        child: Row(children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  image: DecorationImage(
+                    image: AssetImage(AssetsApp.test),
+                    fit: BoxFit.fill,
+                  )),
+            ),
           ),
-        ),
-        const SizedBox(width: 30),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: const Text('Harry Potter and the Goblet of Fire',
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                      style: TextStyle(
-                        fontSize: 6,
-                        fontWeight: FontWeight.bold,
-                      ))),
-              const SizedBox(height: 3),
-              const Text(
-                "J.K. Rowling",
-                style: TextStyle(
-                  fontSize: 6,
-                ),
-              ),
-              const Row(
-                children: [
-                  Text(
-                    "19.99 €",
-                    style: TextStyle(
-                      fontSize: 9,
-                      fontWeight: FontWeight.bold,
-                    ),
+          const SizedBox(width: 30),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: const Text('Harry Potter and the Goblet of Fire',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 6,
+                          fontWeight: FontWeight.bold,
+                        ))),
+                const SizedBox(height: 3),
+                const Text(
+                  "J.K. Rowling",
+                  style: TextStyle(
+                    fontSize: 6,
                   ),
-                  Spacer(),
-                  BookRating(),
-                ],
-              ),
-            ],
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      "19.99 €",
+                      style: TextStyle(
+                        fontSize: 9,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    BookRating(),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
