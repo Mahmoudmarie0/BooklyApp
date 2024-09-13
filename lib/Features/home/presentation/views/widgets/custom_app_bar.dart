e40e0a6d9942bcf6f2333ab7a0edb92d4a8f3691
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-13 16:18:05 +0300

    Search View Added

--- lib/Features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 
@@ -21,7 +23,9 @@ class CustomAppBar extends StatelessWidget {
           ),
           const Spacer(),
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kSearchView);
+              },
               icon: const Icon(
                 FontAwesomeIcons.magnifyingGlass,
                 size: 24,
