3646c0638fca80fdf14371fedc85260e20419d02
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-09-17 13:11:18 +0300

    Create Featured book state

---
+++ lib/Features/home/presentation/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,19 @@
+import '../../domain/entities/book_entity.dart';
+
+abstract class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String message;
+
+  FeaturedBooksFailure(this.message);
+}
+
+class FeaturedBooksSucces extends FeaturedBooksState {
+  List<BookEntity> book;
+
+  FeaturedBooksSucces(this.book);
+}
